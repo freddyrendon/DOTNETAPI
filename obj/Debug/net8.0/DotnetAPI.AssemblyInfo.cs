0044979ab2bc442add70384bf87a42964825c31f
Freddy Rendon <frendonjr03@gmail.com>
2024-06-08 03:16:11 -0400

    .gitignore

--- obj/Debug/net8.0/DotnetAPI.AssemblyInfo.cs
+++ obj/Debug/net8.0/DotnetAPI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DotnetAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c313b949e4f3a89447b2f438d430303a968be2bf")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4ef8a0e5106ecda0600ae3982995bf4a99892188")]
 [assembly: System.Reflection.AssemblyProductAttribute("DotnetAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DotnetAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
